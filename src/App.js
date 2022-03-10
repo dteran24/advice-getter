7b053e2d765f7e884e3294a65e32ed1f5e84c2f9
Daniel <dteran21@yahoo.com>
2022-03-10 12:28:12 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
+
+
+  const RANDOM_ADVICE_URL = 'https://api.adviceslip.com/advice';
+  const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    
+    useEffect(() => {
+      async function getData(){
+        const response = await axios.get(url)
+        setData(response.data.slip)
+        setLoading(false);
+      }
+      getData();
+    },[loading]);
+
+    return {data, loading, setLoading};
+    
+  }
+ 
 
 function App() {
+
+  const {data, loading, setLoading} = useFetch(RANDOM_ADVICE_URL);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='adviceBox'>
+        {loading ? <div>Loading...</div>:
+        <><p>{data.advice}</p><p>#{data.id}</p></>}
+        </div>
+      <button onClick={() => setLoading(true)}>Get Advice</button>
     </div>
   );
 }
